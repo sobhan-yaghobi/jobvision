46b0e6cf4af84477fe6bcb42749d1574127a74e2
unknown <sobhan13831383yaghobi@gmail.com>
2023-09-19 12:16:08 +0330

    fix animation accordion component

--- src/Components/Accordion/Accordion.tsx
+++ src/Components/Accordion/Accordion.tsx
@@ -4,33 +4,32 @@ import { AiOutlineDown } from "react-icons/ai";
 interface AccordionProps {
     title: string;
     content: string;
-    isOpen?: boolean;
+    isOpen?: boolean | undefined;
 }
 
 const Accordion: React.FC<AccordionProps> = ({ title, content, isOpen }) => {
-    const [isActive, setIsActive] = useState<boolean>(isOpen ? isOpen : false);
+    const [isActive, setIsActive] = useState<boolean>(typeof isOpen === "undefined" ? false : isOpen);
     return (
         <div
-            className={`accordion-item transition-all duration-300 my-1 w-full border border-solid rounded-xl p-1 border-transparent ${
-                isActive ? "border-jv-dark" : ""
+            className={`accordion-item transition-all duration-300 my-1 w-full border border-solid rounded-xl p-1 ${
+                isActive ? "border-jv-dark" : "border-transparent"
             }`}
         >
             <div
                 onClick={() => setIsActive((prev) => !prev)}
-                className={`accordion-header py-2 px-3 flex items-center justify-between transition-all duration-300 ${
-                    isActive ? "text-jv-primary" : "border-b-[1px] border-solid"
+                className={`accordion-header py-2 px-3 flex items-center justify-between transition-all duration-300 border-b-[1px] border-solid ${
+                    isActive ? "text-jv-primary border-jv-primary" : "border-jv-dark"
                 }`}
             >
                 <span className="truncate text-xl">{title}</span>
                 <AiOutlineDown className={`${isActive ? "rotate-180" : ""}`}></AiOutlineDown>
             </div>
-            {/* <div className={`${isActive ? "block" : "hidden"} border border-solid border-jv-primary my-3`}></div> */}
             <div
                 className={`wrapper overflow-hidden grid transition-all duration-700 ${
-                    isActive ? "grid-rows-1" : "grid-rows-[0fr]"
+                    isActive ? "grid-rows-[1fr] py-5" : "grid-rows-[0fr]"
                 }`}
             >
-                <div className={`accordion-content p-2 relative transition-all duration-300 h-full`}>{content}</div>
+                <div className={`min-h-0`}>{content}</div>
             </div>
         </div>
     );
