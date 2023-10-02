746b64ceb37e6f78129659c1748169ea0e557dc8
unknown <sobhan13831383yaghobi@gmail.com>
2023-10-02 09:47:14 +0330

    overWrite Type of FilterJobs and Testing Mode

--- src/Components/SearchFrom/SearchForm.type.ts
+++ src/Components/SearchFrom/SearchForm.type.ts
@@ -1,13 +1,14 @@
 import uuidGenerator from "../../Utils/UuidGenerator";
 
+type TYPE_OF_COOPERTION = "TYPE_OF_COOPERTION"; //!
+type RIGHTS = "RIGHTS"; //!
+type PUBLICATION_DATE = "PUBLICATION_DATE"; //!
+type WORK_EXPERIENCE = "WORK_EXPERIENCE"; //!
+type SENIORITY_LEVEL = "SENIORITY_LEVEL"; //!
+type BENEFITS_AND_FACILITIES = "BENEFITS_AND_FACILITIES"; //!
+
 type TELECOMMUTING = "TELECOMMUTING";
 type INTERSHIP = "INTERSHIP";
-type TYPE_OF_COOPERTION = "TYPE_OF_COOPERTION";
-type RIGHTS = "RIGHTS";
-type PUBLICATION_DATE = "PUBLICATION_DATE";
-type WORK_EXPERIENCE = "WORK_EXPERIENCE";
-type SENIORITY_LEVEL = "SENIORITY_LEVEL";
-type BENEFITS_AND_FACILITIES = "BENEFITS_AND_FACILITIES";
 type IS_EMPLOYMENT_OF_THE_DISABLED = "IS_EMPLOYMENT_OF_THE_DISABLED";
 type MILITARY_ORDER = "MILITARY_ORDER";
 
@@ -44,116 +45,87 @@ type BENEFITS_AND_FACILITIES_LOAN = "BENEFITS_AND_FACILITIES_LOAN";
 type BENEFITS_AND_FACILITIES_REWARD = "BENEFITS_AND_FACILITIES_REWARD";
 type BENEFITS_AND_FACILITIES_PARKING = "BENEFITS_AND_FACILITIES_PARKING";
 
-type TypeAndTitleOfFilterTypes =
-    | {
-          title: "دورکاری";
-          type: TELECOMMUTING;
-      }
-    | {
-          title: "کارآموزی";
-          type: INTERSHIP;
-      }
-    | {
-          title: "نوع همکاری";
-          type: TYPE_OF_COOPERTION;
-      }
-    | {
-          title: "حقوق";
-          type: RIGHTS;
-      }
-    | {
-          title: "زمان انتشار";
-          type: PUBLICATION_DATE;
-      }
-    | {
-          title: "سابقه کاری";
-          type: WORK_EXPERIENCE;
-      }
-    | {
-          title: "سطح ارشدیت";
-          type: SENIORITY_LEVEL;
-      }
-    | {
-          title: "مزایا و تسهیلات";
-          type: BENEFITS_AND_FACILITIES;
-      }
-    | {
-          title: "امکان استخدام معلولین";
-          type: IS_EMPLOYMENT_OF_THE_DISABLED;
-      }
-    | {
-          title: "امریه سربازی";
-          type: MILITARY_ORDER;
-      };
+export type CategoryTypes =
+    | TYPE_OF_COOPERTION
+    | RIGHTS
+    | PUBLICATION_DATE
+    | WORK_EXPERIENCE
+    | SENIORITY_LEVEL
+    | BENEFITS_AND_FACILITIES;
 
-type TypeAndTitleOfChildFilterTypes =
-    | { type: TYPE_OF_COOPERTION_FULL_TIME; title: "تمام وقت"; parentType: TYPE_OF_COOPERTION }
-    | { type: TYPE_OF_COOPERTION_CONTRACTUAL_TIME; title: "قراردای / پروژه ای"; parentType: TYPE_OF_COOPERTION }
-    | { type: TYPE_OF_COOPERTION_PART_TIME; title: "نیمه وقت"; parentType: TYPE_OF_COOPERTION }
-    | { type: RIGHTS_UNDER_4_MILLION; title: "زیر 4 میلیون تومان"; parentType: RIGHTS }
-    | { type: RIGHTS_AMONG_4_8_MILLION; title: "بین 4 تا 8 میلیون تومان"; parentType: RIGHTS }
-    | { type: RIGHTS_AMONG_8_12_MILLION; title: "بین 8 تا 12 میلیون تومان"; parentType: RIGHTS }
-    | { type: RIGHTS_AMONG_12_20_MILLION; title: "بین 12 تا 20 میلیون تومان"; parentType: RIGHTS }
-    | { type: RIGHTS_OVER_60_MILLION; title: "بالای 60 میلیون تومان"; parentType: RIGHTS }
-    | { type: PUBLICATION_DATE_LAST_3_DAY; title: "3 روز اخیر"; parentType: PUBLICATION_DATE }
-    | { type: PUBLICATION_DATE_LAST_1_WEEK; title: "1 هفته اخیر"; parentType: PUBLICATION_DATE }
-    | { type: PUBLICATION_DATE_LAST_15_DAY; title: "15 روز اخیر"; parentType: PUBLICATION_DATE }
-    | { type: PUBLICATION_DATE_LAST_1_MONTH; title: "1 ماه اخیر"; parentType: PUBLICATION_DATE }
-    | { type: WORK_EXPERIENCE_UNDER_2_YR; title: "کمتر از 2 سال"; parentType: WORK_EXPERIENCE }
-    | { type: WORK_EXPERIENCE_AMONG_2_5_YR; title: "بین 2 تا 5 سال"; parentType: WORK_EXPERIENCE }
-    | { type: WORK_EXPERIENCE_AMONG_5_8_YR; title: "بین 5 تا 8 سال"; parentType: WORK_EXPERIENCE }
-    | { type: WORK_EXPERIENCE_AMONG_8_12_YR; title: "بین 8 تا 12 سال"; parentType: WORK_EXPERIENCE }
-    | { type: WORK_EXPERIENCE_OVER_12_YR; title: "بالای 12 سال"; parentType: WORK_EXPERIENCE }
-    | { type: SENIORITY_LEVEL_MANUAL_WORKER; title: "کارگر"; parentType: SENIORITY_LEVEL }
-    | { type: SENIORITY_LEVEL_EMPLOYEE; title: "کارمند"; parentType: SENIORITY_LEVEL }
-    | { type: SENIORITY_LEVEL_EXPERT; title: "کارشناس"; parentType: SENIORITY_LEVEL }
-    | { type: SENIORITY_LEVEL_MA; title: "کارشناس ارشد"; parentType: SENIORITY_LEVEL }
-    | { type: SENIORITY_LEVEL_MID_LEVEL_MANAGER; title: "مدیر میانی"; parentType: SENIORITY_LEVEL }
-    | { type: SENIORITY_LEVEL_ASSISTANCE; title: "معاونت"; parentType: SENIORITY_LEVEL }
-    | { type: SENIORITY_LEVEL_CHIEF; title: "مدیرارشد"; parentType: SENIORITY_LEVEL }
-    | { type: BENEFITS_AND_FACILITIES_LOAN; title: "وام"; parentType: BENEFITS_AND_FACILITIES }
-    | { type: BENEFITS_AND_FACILITIES_REWARD; title: "پاداش"; parentType: BENEFITS_AND_FACILITIES }
-    | { type: BENEFITS_AND_FACILITIES_PARKING; title: "پارکینگ"; parentType: BENEFITS_AND_FACILITIES };
+export type FiltreTypes =
+    | TELECOMMUTING
+    | INTERSHIP
+    | IS_EMPLOYMENT_OF_THE_DISABLED
+    | MILITARY_ORDER
+    | TYPE_OF_COOPERTION_FULL_TIME
+    | TYPE_OF_COOPERTION_CONTRACTUAL_TIME
+    | TYPE_OF_COOPERTION_PART_TIME
+    | RIGHTS_UNDER_4_MILLION
+    | RIGHTS_AMONG_4_8_MILLION
+    | RIGHTS_AMONG_8_12_MILLION
+    | RIGHTS_AMONG_12_20_MILLION
+    | RIGHTS_OVER_60_MILLION
+    | PUBLICATION_DATE_LAST_3_DAY
+    | PUBLICATION_DATE_LAST_1_WEEK
+    | PUBLICATION_DATE_LAST_15_DAY
+    | PUBLICATION_DATE_LAST_1_MONTH
+    | WORK_EXPERIENCE_UNDER_2_YR
+    | WORK_EXPERIENCE_AMONG_2_5_YR
+    | WORK_EXPERIENCE_AMONG_5_8_YR
+    | WORK_EXPERIENCE_AMONG_8_12_YR
+    | WORK_EXPERIENCE_OVER_12_YR
+    | SENIORITY_LEVEL_MANUAL_WORKER
+    | SENIORITY_LEVEL_EMPLOYEE
+    | SENIORITY_LEVEL_EXPERT
+    | SENIORITY_LEVEL_MA
+    | SENIORITY_LEVEL_MID_LEVEL_MANAGER
+    | SENIORITY_LEVEL_ASSISTANCE
+    | SENIORITY_LEVEL_CHIEF
+    | BENEFITS_AND_FACILITIES_LOAN
+    | BENEFITS_AND_FACILITIES_REWARD
+    | BENEFITS_AND_FACILITIES_PARKING;
 
-interface ChildOfFilterMainType {
-    id: string;
-}
-
-export type ChildOfFilterType = ChildOfFilterMainType & TypeAndTitleOfChildFilterTypes;
-
-interface FilterMainType {
+export type ChildOfFilterType = {
     id: string;
     title: string;
+    type: FiltreTypes;
+    category: CategoryTypes;
+};
+
+export interface FilterType {
+    id: string;
+    title: string;
+    type?: FiltreTypes;
     isMultiple: boolean;
     sub?: ChildOfFilterType | ChildOfFilterType[];
+    category?: CategoryTypes;
 }
-
-export type FilterType = FilterMainType & TypeAndTitleOfFilterTypes;
 
 const categoryArray: FilterType[] = [
     { id: uuidGenerator(), type: "TELECOMMUTING", title: "دورکاری", isMultiple: false },
     { id: uuidGenerator(), type: "INTERSHIP", title: "کارآموزی", isMultiple: false },
     {
         id: uuidGenerator(),
-        type: "TYPE_OF_COOPERTION",
         title: "نوع همکاری",
+        category: "TYPE_OF_COOPERTION",
         isMultiple: false,
         sub: [
             {
                 id: uuidGenerator(),
-                parentType: "TYPE_OF_COOPERTION",
+                category: "TYPE_OF_COOPERTION",
                 title: "تمام وقت",
                 type: "TYPE_OF_COOPERTION_FULL_TIME",
             },
             {
                 id: uuidGenerator(),
-                parentType: "TYPE_OF_COOPERTION",
+                category: "TYPE_OF_COOPERTION",
                 title: "نیمه وقت",
                 type: "TYPE_OF_COOPERTION_PART_TIME",
             },
             {
                 id: uuidGenerator(),
-                parentType: "TYPE_OF_COOPERTION",
+                category: "TYPE_OF_COOPERTION",
                 title: "قراردای / پروژه ای",
                 type: "TYPE_OF_COOPERTION_CONTRACTUAL_TIME",
             },
@@ -161,32 +133,32 @@ const categoryArray: FilterType[] = [
     },
     {
         id: uuidGenerator(),
-        type: "RIGHTS",
+        category: "RIGHTS",
         title: "حقوق",
         isMultiple: false,
         sub: [
-            { id: uuidGenerator(), parentType: "RIGHTS", title: "زیر 4 میلیون تومان", type: "RIGHTS_UNDER_4_MILLION" },
+            { id: uuidGenerator(), category: "RIGHTS", title: "زیر 4 میلیون تومان", type: "RIGHTS_UNDER_4_MILLION" },
             {
                 id: uuidGenerator(),
-                parentType: "RIGHTS",
+                category: "RIGHTS",
                 title: "بین 4 تا 8 میلیون تومان",
                 type: "RIGHTS_AMONG_4_8_MILLION",
             },
             {
                 id: uuidGenerator(),
-                parentType: "RIGHTS",
+                category: "RIGHTS",
                 title: "بین 8 تا 12 میلیون تومان",
                 type: "RIGHTS_AMONG_8_12_MILLION",
             },
             {
                 id: uuidGenerator(),
-                parentType: "RIGHTS",
+                category: "RIGHTS",
                 title: "بین 12 تا 20 میلیون تومان",
                 type: "RIGHTS_AMONG_12_20_MILLION",
             },
             {
                 id: uuidGenerator(),
-                parentType: "RIGHTS",
+                category: "RIGHTS",
                 title: "بالای 60 میلیون تومان",
                 type: "RIGHTS_OVER_60_MILLION",
             },
@@ -194,31 +166,31 @@ const categoryArray: FilterType[] = [
     },
     {
         id: uuidGenerator(),
-        type: "PUBLICATION_DATE",
+        category: "PUBLICATION_DATE",
         title: "زمان انتشار",
         isMultiple: false,
         sub: [
             {
                 id: uuidGenerator(),
-                parentType: "PUBLICATION_DATE",
+                category: "PUBLICATION_DATE",
                 title: "3 روز اخیر",
                 type: "PUBLICATION_DATE_LAST_3_DAY",
             },
             {
                 id: uuidGenerator(),
-                parentType: "PUBLICATION_DATE",
+                category: "PUBLICATION_DATE",
                 title: "1 هفته اخیر",
                 type: "PUBLICATION_DATE_LAST_1_WEEK",
             },
             {
                 id: uuidGenerator(),
-                parentType: "PUBLICATION_DATE",
+                category: "PUBLICATION_DATE",
                 title: "15 روز اخیر",
                 type: "PUBLICATION_DATE_LAST_15_DAY",
             },
             {
                 id: uuidGenerator(),
-                parentType: "PUBLICATION_DATE",
+                category: "PUBLICATION_DATE",
                 title: "1 ماه اخیر",
                 type: "PUBLICATION_DATE_LAST_1_MONTH",
             },
@@ -226,37 +198,37 @@ const categoryArray: FilterType[] = [
     },
     {
         id: uuidGenerator(),
-        type: "WORK_EXPERIENCE",
+        category: "WORK_EXPERIENCE",
         title: "سابقه کاری",
         isMultiple: false,
         sub: [
             {
                 id: uuidGenerator(),
-                parentType: "WORK_EXPERIENCE",
+                category: "WORK_EXPERIENCE",
                 title: "کمتر از 2 سال",
                 type: "WORK_EXPERIENCE_UNDER_2_YR",
             },
             {
                 id: uuidGenerator(),
-                parentType: "WORK_EXPERIENCE",
+                category: "WORK_EXPERIENCE",
                 title: "بین 2 تا 5 سال",
                 type: "WORK_EXPERIENCE_AMONG_2_5_YR",
             },
             {
                 id: uuidGenerator(),
-                parentType: "WORK_EXPERIENCE",
+                category: "WORK_EXPERIENCE",
                 title: "بین 5 تا 8 سال",
                 type: "WORK_EXPERIENCE_AMONG_5_8_YR",
             },
             {
                 id: uuidGenerator(),
-                parentType: "WORK_EXPERIENCE",
+                category: "WORK_EXPERIENCE",
                 title: "بین 8 تا 12 سال",
                 type: "WORK_EXPERIENCE_AMONG_8_12_YR",
             },
             {
                 id: uuidGenerator(),
-                parentType: "WORK_EXPERIENCE",
+                category: "WORK_EXPERIENCE",
                 title: "بالای 12 سال",
                 type: "WORK_EXPERIENCE_OVER_12_YR",
             },
@@ -264,50 +236,50 @@ const categoryArray: FilterType[] = [
     },
     {
         id: uuidGenerator(),
-        type: "SENIORITY_LEVEL",
+        category: "SENIORITY_LEVEL",
         title: "سطح ارشدیت",
         isMultiple: false,
         sub: [
             {
                 id: uuidGenerator(),
-                parentType: "SENIORITY_LEVEL",
+                category: "SENIORITY_LEVEL",
                 title: "کارگر",
                 type: "SENIORITY_LEVEL_MANUAL_WORKER",
             },
-            { id: uuidGenerator(), parentType: "SENIORITY_LEVEL", title: "کارمند", type: "SENIORITY_LEVEL_EMPLOYEE" },
-            { id: uuidGenerator(), parentType: "SENIORITY_LEVEL", title: "کارشناس", type: "SENIORITY_LEVEL_EXPERT" },
-            { id: uuidGenerator(), parentType: "SENIORITY_LEVEL", title: "کارشناس ارشد", type: "SENIORITY_LEVEL_MA" },
+            { id: uuidGenerator(), category: "SENIORITY_LEVEL", title: "کارمند", type: "SENIORITY_LEVEL_EMPLOYEE" },
+            { id: uuidGenerator(), category: "SENIORITY_LEVEL", title: "کارشناس", type: "SENIORITY_LEVEL_EXPERT" },
+            { id: uuidGenerator(), category: "SENIORITY_LEVEL", title: "کارشناس ارشد", type: "SENIORITY_LEVEL_MA" },
             {
                 id: uuidGenerator(),
-                parentType: "SENIORITY_LEVEL",
+                category: "SENIORITY_LEVEL",
                 title: "مدیر میانی",
                 type: "SENIORITY_LEVEL_MID_LEVEL_MANAGER",
             },
-            { id: uuidGenerator(), parentType: "SENIORITY_LEVEL", title: "معاونت", type: "SENIORITY_LEVEL_ASSISTANCE" },
-            { id: uuidGenerator(), parentType: "SENIORITY_LEVEL", title: "مدیرارشد", type: "SENIORITY_LEVEL_CHIEF" },
+            { id: uuidGenerator(), category: "SENIORITY_LEVEL", title: "معاونت", type: "SENIORITY_LEVEL_ASSISTANCE" },
+            { id: uuidGenerator(), category: "SENIORITY_LEVEL", title: "مدیرارشد", type: "SENIORITY_LEVEL_CHIEF" },
         ],
     },
     {
         id: uuidGenerator(),
-        type: "BENEFITS_AND_FACILITIES",
+        category: "BENEFITS_AND_FACILITIES",
         title: "مزایا و تسهیلات",
         isMultiple: true,
         sub: [
             {
                 id: uuidGenerator(),
-                parentType: "BENEFITS_AND_FACILITIES",
+                category: "BENEFITS_AND_FACILITIES",
                 title: "وام",
                 type: "BENEFITS_AND_FACILITIES_LOAN",
             },
             {
                 id: uuidGenerator(),
-                parentType: "BENEFITS_AND_FACILITIES",
+                category: "BENEFITS_AND_FACILITIES",
                 title: "پاداش",
                 type: "BENEFITS_AND_FACILITIES_REWARD",
             },
             {
                 id: uuidGenerator(),
-                parentType: "BENEFITS_AND_FACILITIES",
+                category: "BENEFITS_AND_FACILITIES",
                 title: "پارکینگ",
                 type: "BENEFITS_AND_FACILITIES_PARKING",
             },
--- src/Components/SearchFrom/SearchFrom.tsx
+++ src/Components/SearchFrom/SearchFrom.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 //  Types
-import { ChildOfFilterType, FilterType } from "./SearchForm.type";
+import { CategoryTypes, ChildOfFilterType, FilterType, FiltreTypes } from "./SearchForm.type";
 import { SearchFromProps, categoryArray } from "./SearchForm.type";
 
 // Animations
-import { ShowAndHideOpacity_Ex, ShowFromRight, ShowOpacity } from "../../Animations/UtilsAnimation";
+import { ShowAndHideOpacity_Ex } from "../../Animations/UtilsAnimation";
 
 // Functions
-import { find } from "lodash";
+import { find, includes } from "lodash";
 
 // Components
 import Input from "../Input/Input";
@@ -15,12 +15,18 @@ import Button from "../Button/Button";
 import { motion, AnimatePresence } from "framer-motion";
 
 // Icons
-import { AiOutlineDown } from "react-icons/ai";
+import { AiOutlineDown, AiTwotonePropertySafety } from "react-icons/ai";
 import { isArray } from "lodash";
 import { AiOutlineClose } from "react-icons/ai";
 
+interface selectedFiltersType {
+    title: string;
+    type: string;
+    category: string;
+}
+
 const SearchFrom: React.FC<SearchFromProps> = ({ isFilterBarShow }) => {
-    const [filterJob, setFilterJob] = useState<(FilterType | ChildOfFilterType)[]>([]);
+    const [selectedFilters, setSelectedFilterss] = useState<selectedFiltersType[]>([]);
     const [mainFilterMenu, setMainFilterMenu] = useState<{
         data: FilterType;
         position: { x: number | undefined; y: number | undefined; width: number | undefined };
@@ -29,25 +35,50 @@ const SearchFrom: React.FC<SearchFromProps> = ({ isFilterBarShow }) => {
         position: { x: undefined, y: undefined, width: undefined },
     });
 
-    const filterAction = (Item: FilterType, event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
-        console.log(Item);
-        if (!Item.isMultiple && typeof Item.sub === "undefined") {
-            setFilterJob((prev) => {
-                if (find(prev, Item)) {
-                    return [...prev.filter((filterItem) => filterItem.id !== Item.id)];
+    const diActiveMenu = () =>
+        setMainFilterMenu({ data: {} as FilterType, position: { x: undefined, y: undefined, width: undefined } });
+
+    const filterAction = (
+        Item: FilterType,
+        SubItem: ChildOfFilterType | undefined,
+        event: React.MouseEvent<HTMLSpanElement, MouseEvent>
+    ) => {
+        setSelectedFilterss((prev) => {
+            if (!Item.isMultiple && typeof Item.sub === "undefined" && typeof Item.type !== "undefined") {
+                if (goCheck(Item.type)) {
+                    console.log("Filter");
+                    return [...prev.filter((filterItem) => filterItem.type !== Item.type)];
                 } else {
-                    return [...prev, { ...Item }];
+                    console.log("NEw ITEN");
+                    return [
+                        ...prev,
+                        { title: Item.title, type: Item.type, category: Item.category ? Item.category : "" },
+                    ];
                 }
-            });
-        } else {
-            console.log("nooooo");
-        }
+            } else {
+                if (!Item.isMultiple && typeof Item.sub !== "undefined" && typeof SubItem !== "undefined") {
+                    if (goCheckCategory(SubItem?.category)) {
+                        return [...prev.filter((filterItem) => filterItem.category !== Item.category)];
+                    } else {
+                        return [...prev, { title: SubItem.title, type: SubItem.type, category: SubItem.category }];
+                    }
+                } else if (Item.isMultiple && typeof Item.sub !== "undefined" && typeof SubItem !== "undefined") {
+                    if (prev.some((filterItem) => filterItem.type === SubItem.type)) {
+                        return [...prev.filter((filterItem) => filterItem.type !== SubItem.type)];
+                    } else {
+                        return [...prev, { title: SubItem.title, type: SubItem.type, category: SubItem.category }];
+                    }
+                }
+
+                return [...prev];
+            }
+        });
 
         // FOR MENU
         const isExsist = Boolean(mainFilterMenu.data.id === Item.id);
-        if (isExsist) {
-            setMainFilterMenu({ data: {} as FilterType, position: { x: undefined, y: undefined, width: undefined } });
-        } else {
+        if (isExsist && !Item.isMultiple) {
+            diActiveMenu();
+        } else if (!isExsist) {
             const elmPosition = event.currentTarget.getBoundingClientRect();
             setMainFilterMenu({
                 data: Item,
@@ -56,9 +87,106 @@ const SearchFrom: React.FC<SearchFromProps> = ({ isFilterBarShow }) => {
         }
     };
 
+    type removeFilterActionTypes =
+        | { mode: "RemoveType"; mainType: string }
+        | { mode: "RemoveCategory"; mainType: string };
+
+    const removeFilterAction = (param: removeFilterActionTypes) => {
+        let newSelectedFilters;
+        if (param.mode === "RemoveType") {
+            newSelectedFilters = selectedFilters.filter((selectedFilter) => selectedFilter.type !== param.mainType);
+        } else if (param.mode === "RemoveCategory") {
+            newSelectedFilters = selectedFilters.filter((selectedFilter) => selectedFilter.category !== param.mainType);
+        } else {
+            newSelectedFilters = [...selectedFilters];
+        }
+        setSelectedFilterss(newSelectedFilters);
+        diActiveMenu();
+    };
+
     useEffect(() => {
-        console.log(filterJob);
-    }, [filterJob]);
+        console.log(selectedFilters);
+    }, [selectedFilters]);
+
+    const goCheck = (ItemType: FiltreTypes | undefined): boolean => {
+        if (typeof ItemType !== "undefined") {
+            const isTrue: boolean[] = selectedFilters.map((selectedFilter) => {
+                if (selectedFilter.type === ItemType) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+            if (includes(isTrue, true)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    };
+
+    const goCheckCategory = (ItemType: CategoryTypes | undefined): boolean => {
+        if (typeof ItemType !== "undefined") {
+            const isTrue: boolean[] = selectedFilters.map((selectedFilter) => {
+                if (selectedFilter.category === ItemType) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+            if (includes(isTrue, true)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    };
+
+    interface GoGetCategoryProps {
+        isMultiple: boolean;
+        Item?: FilterType;
+        ItemType: CategoryTypes;
+    }
+
+    const GoGetCategory: React.FC<GoGetCategoryProps> = ({ isMultiple, Item, ItemType }) => {
+        console.log(ItemType, isMultiple);
+        const mainFilterSelect = selectedFilters.filter((selectedFilter) => selectedFilter.category === ItemType);
+
+        if (isMultiple && typeof Item !== "undefined") {
+            return (
+                <>
+                    <span>{Item.title}</span>
+                    <span className="mx-2">{mainFilterSelect.length}</span>
+                    <span
+                        onClick={() =>
+                            removeFilterAction({ mode: "RemoveCategory", mainType: mainFilterSelect[0].category })
+                        }
+                        className="flex items-center"
+                    >
+                        <AiOutlineClose className="mr-1 text-jv-white text-xl" />
+                    </span>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <span className="text-jv-white ml-1">{mainFilterSelect[0].title}</span>
+                    <span
+                        onClick={() => removeFilterAction({ mode: "RemoveType", mainType: mainFilterSelect[0].type })}
+                        className="flex items-center"
+                    >
+                        <AiOutlineClose className="mr-1 text-jv-white text-xl" />
+                    </span>
+                </>
+            );
+        }
+    };
 
     return (
         <>
@@ -156,16 +284,30 @@ const SearchFrom: React.FC<SearchFromProps> = ({ isFilterBarShow }) => {
                     {categoryArray.map((item) => (
                         <div
                             key={item.id}
-                            onClick={(e) => filterAction(item, e)}
+                            onClick={(e) => {
+                                if (!goCheckCategory(item.category)) {
+                                    filterAction(item, undefined, e);
+                                }
+                            }}
                             className={`filtredItem select-none box-info-type text-sm flex items-center px-4 py-2 rounded-2xl cursor-pointer relative ${
-                                find(filterJob, item) ? "bg-jv-lightGray2x border-jv-lightGray2x" : ""
+                                goCheck(item.type) || goCheckCategory(item.category)
+                                    ? "text-jv-white bg-jv-primary"
+                                    : ""
                             }`}
                         >
-                            {find(filterJob, item) ? (
+                            {goCheck(item.type) ? (
                                 <>
                                     <span className="text-jv-white ml-1">{item.title}</span>
-                                    <AiOutlineClose className="mr-1 bg-jv-primary text-jv-white rounded-full text-xl py-1" />
+                                    <AiOutlineClose className="mr-1 text-jv-white text-xl" />
                                 </>
+                            ) : typeof item.category !== "undefined" &&
+                              goCheckCategory(item.category) &&
+                              !item.isMultiple ? (
+                                <GoGetCategory isMultiple={false} ItemType={item.category}></GoGetCategory>
+                            ) : typeof item.category !== "undefined" &&
+                              goCheckCategory(item.category) &&
+                              item.isMultiple ? (
+                                <GoGetCategory isMultiple={true} ItemType={item.category} Item={item}></GoGetCategory>
                             ) : (
                                 <>
                                     <span>{item.title}</span>
@@ -200,8 +342,24 @@ const SearchFrom: React.FC<SearchFromProps> = ({ isFilterBarShow }) => {
                             >
                                 <ul className="absolute min-w-[15rem] top-3 right-0 bg-jv-light py-3 px-4 shadow-xl border-[1px] border-solid border-jv-lightGray3x rounded-xl">
                                     {mainFilterMenu.data.sub.map((subItem) => (
-                                        <li key={subItem.id} className="py-2">
-                                            {subItem.title}
+                                        <li
+                                            onClick={(e) => filterAction(mainFilterMenu.data, subItem, e)}
+                                            key={subItem.id}
+                                            className="py-2 flex items-center cursor-pointer"
+                                        >
+                                            <div
+                                                className={`${
+                                                    goCheck(subItem.type) ? "bg-jv-primary" : "bg-jv-lightGray3x"
+                                                }`}
+                                            >
+                                                {mainFilterMenu.data.isMultiple ? (
+                                                    <div className="w-4 h-4 bg-inherit rounded-sm"></div>
+                                                ) : (
+                                                    <div className="w-4 h-4 bg-inherit rounded-full"></div>
+                                                )}
+                                            </div>
+
+                                            <span className="mr-2">{subItem.title}</span>
                                         </li>
                                     ))}
                                 </ul>
