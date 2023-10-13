c58bb46b59a281b14d4dfefe6a470e6097c849dc
unknown <sobhan13831383yaghobi@gmail.com>
2023-10-13 16:21:55 +0330

    build Menu Of Cms And Set type for cms Pages

--- src/Page/CmsEmployer/CmsEmployer.tsx
+++ src/Page/CmsEmployer/CmsEmployer.tsx
@@ -1,106 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import uuidGenerator from "../../Utils/UuidGenerator";
-import Button from "../../Components/Button/Button";
+// Types
+import { CmsMenuItem, LiteralsMainPage } from "./CmsEmployer.type";
 
-import { AiOutlineMenuUnfold } from "react-icons/ai";
-import { IconType } from "react-icons";
-import { BsFillCaretDownFill } from "react-icons/bs";
+import { Menu } from "antd";
+import { motion, AnimatePresence } from "framer-motion";
+
 import { GoHomeFill } from "react-icons/go";
 import { CgFileDocument } from "react-icons/cg";
-import { MdOutlineInsertChartOutlined } from "react-icons/md";
+import { BiGitPullRequest } from "react-icons/bi";
+import { BsCheckAll } from "react-icons/bs";
+import { RxLapTimer } from "react-icons/rx";
+import { TbGitPullRequestClosed } from "react-icons/tb";
+import { RiGitPullRequestFill } from "react-icons/ri";
 
-interface TypeMenuList {
-    id: string;
-    title: string;
-    icon: IconType;
-    subMenu: { id: string; title: string }[];
-}
+const getItem = (
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: CmsMenuItem[],
+    type?: "group"
+): CmsMenuItem => {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    } as CmsMenuItem;
+};
 
-const MenuListArray: TypeMenuList[] = [
-    { id: uuidGenerator(), title: "گزینه 1", icon: GoHomeFill, subMenu: [] },
-    { id: uuidGenerator(), title: "گزینه 2", icon: CgFileDocument, subMenu: [] },
-    {
-        id: uuidGenerator(),
-        title: "گزینه 3",
-        icon: MdOutlineInsertChartOutlined,
-        subMenu: [
-            { id: uuidGenerator(), title: "گزینه 1 of 3" },
-            { id: uuidGenerator(), title: "گزینه 2 of 3" },
-            { id: uuidGenerator(), title: "گزینه 3 of 3" },
-            { id: uuidGenerator(), title: "گزینه 4 of 3" },
-        ],
-    },
+const items: CmsMenuItem[] = [
+    getItem("خانه", LiteralsMainPage.Home, <GoHomeFill />),
+    getItem("آگهی ها", LiteralsMainPage.Advertsisings, <CgFileDocument />),
+    getItem("درخواست ها", "sub1", <BiGitPullRequest />, [
+        getItem("قبول شده", LiteralsMainPage.RqAccept, <BsCheckAll style={{ color: "var(--successColor)" }} />),
+        getItem(
+            "رد شده",
+            LiteralsMainPage.RqRejection,
+            <TbGitPullRequestClosed style={{ color: "var(--dangerColor)" }} />
+        ),
+        getItem("در حال انتظار", LiteralsMainPage.RqWaiting, <RxLapTimer style={{ color: "var(--warningColor)" }} />),
+        getItem("تمامی درخواست ها", LiteralsMainPage.RqAll, <RiGitPullRequestFill />),
+    ]),
 ];
 
-type TypeItemMenu = { id: string; isArrow: boolean; title: string; Icon: IconType };
-
 const CmsEmployer: React.FC = () => {
-    const [isMenuCollapsed, setIsMenuCollapsed] = useState(false);
-    const [mainItem, setMainItem] = useState<TypeMenuList>({} as TypeMenuList);
-    const ItemMenu: React.FC<TypeItemMenu> = ({ id, isArrow, title, Icon }) => {
-        const isActive = typeof mainItem !== "undefined" && Boolean(id === mainItem.id);
-        return (
-            <>
-                {isArrow ? (
-                    <div className="flex items-center justify-between">
-                        <Icon />
-                        <span>{title}</span>
-                        <BsFillCaretDownFill />
-                    </div>
-                ) : (
-                    <></>
-                )}
-            </>
-        );
-    };
+    const [MainPage, setMainPage] = useState<{ mainKey: React.Key }>({ mainKey: LiteralsMainPage.Home });
+
     return (
         <>
-            <div className="w-full h-screen flex justify-between p-4">
-                <div className={`h-full rounded-lg ml-4 bg-jv-lightPrimary ${isMenuCollapsed ? "w-1/12" : "w-2/12"}`}>
-                    <Button
-                        textColor="primary"
-                        ClickHandler={() => setIsMenuCollapsed((prev) => !prev)}
-                        isLoading={false}
-                        IconType="JSX_ICON"
-                        Icon={
-                            <AiOutlineMenuUnfold
-                                className={`text-base ${isMenuCollapsed ? "rotate-180" : "rotate-0"}`}
-                            />
-                        }
-                        size="small"
-                        ClassName={`w-full !px-0`}
-                    ></Button>
-                    <ul className="mt-3 p-1">
-                        {MenuListArray.map((item) => (
-                            <li
-                                className={`cursor-pointer rounded-lg text-sm text-jv-white p-3 mb-1 bg-jv-primary flex items-center`}
-                            >
-                                {item.subMenu.length ? (
-                                    <div className="w-full">
-                                        <span className="flex items-center justify-between">
-                                            <span className="flex items-center">
-                                                <item.icon className="ml-3" />
-                                                {item.title}
-                                            </span>
-                                            <BsFillCaretDownFill />
-                                        </span>
-                                        <ul></ul>
-                                    </div>
-                                ) : (
-                                    <>
-                                        <item.icon />
-                                        <span className="">{item.title}</span>
-                                    </>
-                                )}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className={`h-full rounded-lg bg-jv-light ${isMenuCollapsed ? "w-11/12" : "w-10/12"}`}></div>
+            <div className="w-full h-screen flex justify-between p-4 relative">
+                <Menu
+                    className="w-2/12 p-1 ml-4 rounded-lg"
+                    defaultSelectedKeys={[LiteralsMainPage.Home]}
+                    defaultOpenKeys={["sub1"]}
+                    mode="inline"
+                    items={items}
+                    onSelect={(props) => setMainPage({ mainKey: props.key })}
+                />
+                <AnimatePresence>
+                    <motion.div className={`w-10/12 h-full rounded-lg bg-jv-light`}>
+                        <CmsPageGenerator type={MainPage.mainKey as LiteralsMainPage.AllPage}></CmsPageGenerator>
+                    </motion.div>
+                </AnimatePresence>
             </div>
         </>
     );
 };
 
 export default CmsEmployer;
+
+type CmsPageGeneratorProps = {
+    type: LiteralsMainPage.AllPage;
+};
+
+const CmsPageGenerator: React.FC<CmsPageGeneratorProps> = ({ type }) => {
+    console.log("CmsPageGenerator", type);
+
+    return <></>;
+};
---
+++ src/Page/CmsEmployer/CmsEmployer.type.ts
@@ -0,0 +1,19 @@
+import { MenuProps } from "antd";
+
+export type CmsMenuItem = Required<MenuProps>["items"][number];
+
+export namespace LiteralsMainPage {
+    export type Home = "Home";
+    export type Advertsisings = "Advertsisings";
+    export type Request_Accept = "Request_Accept";
+    export type Request_Rejection = "Request_Rejection";
+    export type Request_Waiting = "Request_Waiting";
+    export type Request_All = "Request_All";
+    export type AllPage = Home | Advertsisings | Request_Accept | Request_Rejection | Request_Waiting | Request_All;
+    export const Home: Home = "Home";
+    export const Advertsisings: Advertsisings = "Advertsisings";
+    export const RqAccept: Request_Accept = "Request_Accept";
+    export const RqRejection: Request_Rejection = "Request_Rejection";
+    export const RqWaiting: Request_Waiting = "Request_Waiting";
+    export const RqAll: Request_All = "Request_All";
+}
